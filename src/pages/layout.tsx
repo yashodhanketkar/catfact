00de30e3d8cb61f75ea46b94bb77ce997a6b8d0f
yashodhanketkar <kykyashodhan@gmail.com>
2023-01-15 18:01:41 +0530

    0.1.0
    1. Created cat fact api.
    2. Added PWA support.

---
+++ src/pages/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Facts from "./facts";
+
+const Layout = () => {
+  return (
+    <div className="content">
+      <Facts />
+    </div>
+  );
+};
+
+export default Layout;
